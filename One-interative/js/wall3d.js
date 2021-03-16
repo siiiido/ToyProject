a61309e959a714be2cdba66860995c5d10570a03
SangHan <discegaudere24.7@gmail.com>
2021-03-16 17:33:24 +0900

    study scrollPer

--- One-interative/js/wall3d.js
+++ One-interative/js/wall3d.js
@@ -7,24 +7,32 @@
   const barElem = document.querySelector('.progress-bar');
   const mousePos = {x:0, y:0};
 
+
+  
+
+
+
   function resizeHandler(){
+    // 전체 문서 높이 - 창 높이  = 전체 스크롤 할 수 있는 범위
     maxScrollValue = document.body.offsetHeight - window.innerHeight;
   }
 
   window.addEventListener('scroll',function(){
-    // console.log(pageYOffset); //스크롤바 y좌표를 출력
-    // console.log(maxScrollValue);
-    // console.log(pageYOffset/maxScrollValue);
 
-    // css house의 translateZ가 -490이라서 그만큼 빼주면 스크롤해도 다음장도 멀리서부터 오게함
-    // 1000으로 꽉 안채우고 980으로 하는것은 끝에서도 입체감 줄려고
+
+    console.log(pageYOffset); //스크롤바 y좌표를 출력
+  // document.body.offsetHeight : 전체 height
+  // window.innerHeight : 한 화면의 height, 즉 스크롤바의 높이임
+
+  //   // css house의 translateZ가 -490이라서 그만큼 빼주면 스크롤해도 다음장도 멀리서부터 오게함
+  //   // 1000으로 꽉 안채우고 980으로 하는것은 끝에서도 입체감 줄려고
+  //   // 0~1사이로 0은 스크롤 움직임x, 1은 스크롤 움직임 끝까지 : 스크롤 움직임의 비율을 나타냄
     const scrollPer = pageYOffset/maxScrollValue ;
     const zMove = scrollPer * 980 - 490;
     houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
 
-
-    // progress bar
-    barElem.style.width = scrollPer * 100 + "%";
+  //   // progress bar
+  //   barElem.style.width = scrollPer * 100 + "%";
   });
 
   window.addEventListener("mousemove",function(e){
@@ -34,6 +42,9 @@
     stageElem.style.transform = 'rotateX('+ (mousePos.y * 5) +'deg) rotateY(' + (mousePos.x * 5) + 'deg)';
   });
 
+
+  // 창 사이즈 바꿔도 거기에 맞게 대응할려고
+  // resize는 함수로 만들어서 사용
   window.addEventListener('resize',resizeHandler);
 
   stageElem.addEventListener('click',function(e){
@@ -44,5 +55,6 @@
 
   });
   resizeHandler();
+
 
 })();
